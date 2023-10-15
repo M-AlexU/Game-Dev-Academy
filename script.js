5788510e684e41a6a41bad06cf85077210bb7d26
M-AlexU <musurelu67@gmail.com>
2023-10-15 23:27:31 +0200

    Add project files

---
+++ script.js
@@ -0,0 +1,32 @@
+const settings = Object.freeze({
+   nav: document.querySelector("nav"),
+   mobileNav: document.querySelector("nav.mobile-nav"),
+   menuIcon: document.querySelector(".menu-icon"),
+   closeIcon: document.querySelector(".close-icon"),
+   mobileMenuContainer: document.querySelector(".mobile-menu-container"),
+   scrollThreshold: 60
+});
+
+window.addEventListener("scroll", () => {
+    const { nav, mobileNav, scrollThreshold } = settings;
+
+   if (window.scrollY > scrollThreshold) {
+        nav.classList.add("scrolled");
+        mobileNav.classList.add("scrolled");
+    } else {
+        nav.classList.remove("scrolled");
+        mobileNav.classList.remove("scrolled");
+    }
+});
+
+menuIcon.addEventListener("pointerdown", () => {
+    const { mobileMenuContainer } = settings;
+
+    mobileMenuContainer.classList.add("active");
+})
+
+closeIcon.addEventListener("pointerdown", () => {
+    const { mobileMenuContainer } = settings;
+
+    mobileMenuContainer.classList.remove("active");
+})
